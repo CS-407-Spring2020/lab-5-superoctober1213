7bb217be52c87743dcc2020ffbfad6be9144a5dd
yuxuan zhang <yzhang2339@wisc.edu>
2020-03-02 09:37:41 -0600

    done

---
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -0,0 +1,93 @@
+package c.sakshi.lab5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView textView;
+    public static ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = (TextView) findViewById(R.id.textView);
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+        textView.setText("Welcome "+ username + "!");
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        notes = dbHelper.readNotes(username);
+
+
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes){
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+            Log.i("debug",note.getTitle() );
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.addNote:
+                Intent addIntent = new Intent(this, Main3Activity.class);
+                startActivity(addIntent);
+                return true;
+
+            case R.id.logout:
+                Intent logoutIntent = new Intent(this, MainActivity.class);
+                SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove("username").apply();
+                startActivity(logoutIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,13 +1,51 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String usernameKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        usernameKey = "username";
+
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+
+        if(!sharedPreferences.getString(usernameKey, "").equals("")){
+            String username = sharedPreferences.getString(usernameKey, "");
+            Intent intent = new Intent(this, Main2Activity.class);
+            intent.putExtra("message", username);
+            startActivity(intent);
+        }else {
+            setContentView(R.layout.activity_main);
+        }
     }
+
+    public void loginFunction(View view){
+        EditText myTextField = (EditText) findViewById(R.id.username);
+        String str = myTextField.getText().toString();
+
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", s).apply();
+
+        startActivity(intent);
+    }
+
+
 }
